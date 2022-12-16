1e3b5547a6251506c528fe995cd18cbb551b58ff
Dorin Tomescu <81182507+foneigten@users.noreply.github.com>
2022-12-16 14:13:28 +0100

    Add files via upload

---
+++ SQL_mit_LINQ/SQL_mit_LINQ/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQL_mit_LINQ
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KontakteDataContext kontext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DatenLaden()
+        {
+            //Kontext erstellen
+            kontext = new KontakteDataContext();
+
+            //die Daten laden
+            var kontakte = from kontakt in kontext.Kontakte
+                           select kontakt;
+
+            //als Quelle setzen
+            KontakteGrid.ItemsSource = kontakte;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DatenLaden();
+        }
+
+        private void buttonNeuerEintrag_Click(object sender, RoutedEventArgs e)
+        {
+            kontext.SubmitChanges();
+        }
+
+        private void buttonAktualisieren_Click(object sender, RoutedEventArgs e)
+        {
+            //die markierte Zeile setzen
+            Kontakte kontaktZeile = KontakteGrid.SelectedItem as Kontakte;
+            
+            //die Nummer beschaffen
+            int suche = kontaktZeile.kNummer;
+            
+            //den Eintrag selektieren
+            Kontakte kontakt = (from eintrag in kontext.Kontakte
+                                where eintrag.kNummer == kontaktZeile.kNummer
+                                select eintrag).Single();
+
+            //die Daten setzen
+            kontakt.nachname = kontaktZeile.nachname;
+            kontakt.vorname = kontaktZeile.vorname;
+            kontakt.postleitzahl = kontaktZeile.postleitzahl;
+            kontakt.ort = kontaktZeile.ort;
+            kontakt.strasse = kontaktZeile.strasse;
+            kontakt.telefon = kontaktZeile.telefon;
+            kontakt.email = kontaktZeile.email;
+
+            //ändern
+            kontext.SubmitChanges();
+
+        }
+
+        private void buttonLoeschen_Click(object sender, RoutedEventArgs e)
+        {
+            //die markierte Zeile setzen
+            Kontakte kontaktZeile = KontakteGrid.SelectedItem as Kontakte;
+
+            //die Nummer beschaffen
+            int suche = kontaktZeile.kNummer;
+
+            //den Eintrag selektieren
+            Kontakte kontakt = (from eintrag in kontext.Kontakte
+                                where eintrag.kNummer == kontaktZeile.kNummer
+                                select eintrag).Single();
+
+            //die Daten löschen
+            kontext.Kontakte.DeleteOnSubmit(kontakt);
+
+            //ändern
+            kontext.SubmitChanges();
+
+            //die Daten neu laden
+            DatenLaden();
+        }
+    }
+}
---
+++ XML_mit_LINQ/XML_mit_LINQ/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace XML_mit_LINQ
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string xmlPfad = "c:\\test\\testliste.xml";
+        XElement bestenliste;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LadenButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bestenliste = XElement.Load(xmlPfad);
+                bestenlisteGrid.DataContext = bestenliste;
+            }
+            catch
+            {
+                MessageBox.Show($"There was a error loading the file", "ERROR", MessageBoxButton.OK);
+            }
+            
+        }
+
+        private void SpeichernButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bestenliste.Save(xmlPfad);
+            }
+            catch
+            {
+                MessageBox.Show($"There was a error saving the file", "ERROR", MessageBoxButton.OK);
+            }
+        }
+
+        private void NeuerEintragButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bestenliste.Add(new XElement("eintrag", new XElement("name", "<leer>"), new XElement("punkte", "<leer>")));
+            }
+            catch
+            {
+                MessageBox.Show($"There was a error Adding a new line to the file ", "ERROR", MessageBoxButton.OK);
+            }
+        }
+        
+    }
+}
